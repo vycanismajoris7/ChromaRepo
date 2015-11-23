304f1b61942a59b32d370f9ddc00672163da05f6
vycanismajoris7 <vycanismajoris7@outlook.com>
2015-11-23 17:34:47 -0600

    First Commit

---
+++ ChromaTest1/ChromaTest1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromaTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an application to test the functionality of the Chroma SDK.");
+            Console.Write("Press Any Key to Continue...");
+            Console.ReadKey();
+        }
+    }
+}
